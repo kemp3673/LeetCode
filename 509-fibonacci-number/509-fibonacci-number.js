2d08d3ab79aa3521ee2ce61eaf0a086e2d63faeb
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-09-15 11:27:26 -0500

    Time: 94 ms (60.51%), Space: 42 MB (43.74%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,27 +3,44 @@
  * @return {number}
  */
 var fib = function(n) {
-    // Check if n is 0, and if so just return 0 right off the bat.
     if (n === 0) {
         return 0;
     }
     
-    // in order to transverse through the sequence we need to track and update what numbers we are currently working with. 
+     
     let a = 0, b = 1, c;
-    // a is the left number, b is the right number, and c is a temporary holder that will allow us to shuffle and increment;
+    
     let num = n - 1;
     while (num > 0) {
-        c = b; // Holds onto previous right value
-        b = a + b // Adds the two numbers and changes right value to the sum of the two
-        a = c; // Replacs left value with previous right value
-        /* 
-        Example
-        [0, 1], 1, 2, 3, 5, 8 <-- We start with the first two numbers 0 & 1
-        On the step:
-        0, [1, 1], 2, 3, 5, 8 <-- Since 0+1=1, the next hightest would be 1 (var b)
-        */
+        c = b;
+        b = a + b
+        a = c;
         num--;
-        // decrement our counter to count down so we stop repeating the process
+        
     }
-    return b; // Return final right (largest) number, has to be c as b is ;
+    return b; 
 };
+
+
+
+/* 
+Check if n is 0, and if so just return 0 right off the bat.
+
+In order to transverse through the sequence we need to track and update what numbers we are currently working with.
+
+To Track, we will use variables a, b & c to track the left value and right values. c is a temporary holder to allow us to update a & bs values/
+
+Var c Previous right value
+Var b Adds the two numbers and changes right value to the sum of the two
+Var a Replaces left value with previous right value
+
+Example:
+        0, 1, 1, 2, 3, 5, 8 <-- example sequence. 
+        [a = 0, b = 1], 1, 2, 3, 5, 8 <-- We start with the first two numbers 0 & 1
+        On the step:
+        0, [a = 1, b = 1], 2, 3, 5, 8 <-- Since 0+1=1, the next hightest would be 1 (var b)
+        
+        
+Decrement our counter to count down so we stop repeating the process
+Return final right (largest) number;
+*/
