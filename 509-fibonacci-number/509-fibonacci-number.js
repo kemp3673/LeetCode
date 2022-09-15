008a851543a2728d267cd43900e4ced7df7e4612
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-09-15 11:20:29 -0500

    Time: 113 ms (37.13%), Space: 41.5 MB (92.27%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,16 +3,27 @@
  * @return {number}
  */
 var fib = function(n) {
+    // Check if n is 0, and if so just return 0 right off the bat.
     if (n === 0) {
         return 0;
     }
+    
+    // in order to transverse through the sequence we need to track and update what numbers we are currently working with. 
     let a = 0, b = 1, c;
+    // a is the left number, b is the right number, and c is a temporary holder that will allow us to shuffle and increment;
     let num = n;
     while (num > 0) {
-        c = b;
-        b = a + b
-        a = c;
+        c = b; // Holds onto previous right value
+        b = a + b // Adds the two numbers and changes right value to the sum of the two
+        a = c; // Replacs left value with previous right value
+        /* 
+        Example
+        [0, 1], 1, 2, 3, 5, 8 <-- We start with the first two numbers 0 & 1
+        On the step:
+        0, [1, 1], 2, 3, 5, 8 <-- Since 0+1=1, the next hightest would be 1 (var b)
+        */
         num--;
+        // decrement our counter to count down so we stop repeating the process
     }
-    return c;
+    return c; // Return final right (largest) number;
 };
