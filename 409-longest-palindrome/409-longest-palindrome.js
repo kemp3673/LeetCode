89d76bf7b8d31aa49fc4827336b1e70410b82253
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-09-15 14:52:18 -0500

    Time: 73 ms (90.73%), Space: 43.1 MB (85.69%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    if (s.length === 1) {
+        return 1;
+    }
+    let count = 0;
+    let singleLetter = 0;
+    let chars = {};
+    for (var i = 0; i < s.length; i++) {
+        if (chars[s[i]]) {
+            chars[s[i]]++
+        } else {
+            chars[s[i]] = 1;
+        }
+    }
+    let unique = Object.keys(chars);
+    for (var j = 0; j < unique.length; j++) {
+        if (chars[unique[j]] / 2 >= 1) {
+            let divisibleTwo = (Math.floor(chars[unique[j]] / 2)*2)
+            count = count + divisibleTwo;
+            chars[unique[j]] = chars[unique[j]] - divisibleTwo;
+            
+        }
+        if (chars[unique[j]] === 1) {
+            singleLetter++;
+        }
+    }
+    if (singleLetter) {
+        count++
+    }
+    return count;
+};
