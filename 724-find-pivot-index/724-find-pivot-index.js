e25827f5e7df9f26c5c4270c431398290f16c6e6
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-09-28 13:40:40 -0500

    Time: 120 ms (59.45%), Space: 44.7 MB (46.29%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -3,29 +3,18 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-        let leftSum = 0; 
-        let rightSum = 0;
-        let result = -1;
-        const findPivot = (pivot) => {
-            pivot = pivot || 0;
-            for (var i = 0; i < nums.length; i++) {
-                if (i < pivot) {
-                    leftSum += nums[i];
-                } else if (i > pivot) {
-                    rightSum += nums[i];
-                }
-            }
-            if (leftSum === rightSum) {
-                    result = pivot;
-            } else if (pivot === nums.length - 1) {
-                return;
-            } else {
-                leftSum = 0;
-                rightSum = 0;
-                pivot++
-                findPivot(pivot);
-            }
-        }
-    findPivot();
-    return result;
+    let totalSum = 0; 
+    let leftSum = 0;
+      
+    for (var i = 0; i < nums.length; i++) {
+        totalSum += nums[i];
+    }
+    
+    for (var k = 0; k < nums.length; k++) {
+        if (leftSum === totalSum - leftSum - nums[k]) {
+            return k;
+        } 
+        leftSum += nums[k];
+    }
+    return -1;      
 };
