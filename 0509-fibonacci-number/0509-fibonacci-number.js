0cd3ad17f159ff0e69536196b8ce5a36f0c5a47d
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-10-12 14:20:20 -0500

    Time: 71 ms (88.94%), Space: 41.5 MB (92.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var fib = function(n) {
-    let fibonacciArray = [0, 1, 1, 2, 3, 5, 8, 13];
-    if (n <= 7) {
+    let fibonacciArray = [0, 1, 1, 2];
+    if (n <= 3) {
         return fibonacciArray[n];
     }
-    for (var i = 8; i <= n; i++) {
+    for (var i = 4; i <= n; i++) {
         fibonacciArray.push(fibonacciArray[i-2] + fibonacciArray[i-1]);
     };
     return fibonacciArray[fibonacciArray.length - 1]
