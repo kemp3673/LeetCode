e5d9a2458c8a061b1a193704332a15979deaea85
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-10-12 15:37:40 -0500

    Time: 63 ms (97.75%), Space: 45.2 MB (31.25%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let result = -1;
+    let position = 0;
+    if (nums.length === 1 && nums[0] === target) {
+        return 0;
+    }
+    while (nums.length > 1) {
+        let mid = Math.floor(nums.length / 2);
+        if (nums[mid] === target) {
+            return position + mid;
+        } else if (nums[mid] < target) {
+            nums.splice(0, mid);
+            position += mid;
+        } else {
+            nums.splice(mid, nums.length - mid);
+            if (nums.length === 1 && nums[0] === target) {
+                return position;
+            }
+        }
+    }
+    return result;
+};
