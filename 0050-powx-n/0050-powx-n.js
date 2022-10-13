7d800e2b19055ba3cfbdf6f173e15ba4d18b7942
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-10-13 16:26:52 -0500

    Time: 87 ms (70.84%), Space: 42.4 MB (26.40%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -3,6 +3,16 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function(x, n) {
-    return Math.pow(x, n);
+var myPow = function(x, n) {   
+    if (n < 0) {
+        return myPow(1/x, Math.abs(n));
+    };
+    if ( n === 0 ) { return 1 };
+    if ( n === 1 ) { return x };
+   
+    if ( n % 2 === 0 ) {
+        return myPow( x * x, n / 2 );
+    } else {
+        return myPow(x, n - 1) * x
+    }
 };
