c77aa7d8d9dce21e5b9ea9fde3ac8b3dba1af3f1
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-10-15 16:41:18 -0500

    Time: 180 ms (23.02%), Space: 45.4 MB (9.81%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,7 +5,7 @@
 var removeDuplicates = function(nums) {
     let i = 0;
     while (i < nums.length) {
-        if (nums[i] === nums[i+1]) {
+        if (nums[i] === nums[i-1]) {
             nums.splice(i, 1);
         } else {
             i++;
