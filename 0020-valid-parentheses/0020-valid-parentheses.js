56f04b4aed9d2124f1a081f5a1074f29fb1922ae
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2022-10-15 23:14:54 -0500

    Time: 105 ms (48.07%), Space: 42.9 MB (27.23%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+      var stack = [];
+  var map = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+  }
+  
+  for (var i = 0; i < s.length; i++) {
+      if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+          stack.push(s[i]);
+      } else {
+          var last = stack.pop();
+          
+          if (s[i] !== map[last]) {return false;}
+      }
+  }
+  
+  if (stack.length !== 0) {return false;}
+  
+  return true;
+};
