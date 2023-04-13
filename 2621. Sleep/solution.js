f47a781a09ace48c96a0fa5be2da4d117ae752e4
NICHOLAS A KEMPKES <nicholas.kempkes@gmail.com>
2023-04-13 11:27:07 -0500

    completed problem(s) #2619-2621, #2626

---
+++ 2621. Sleep/solution.js
@@ -0,0 +1,44 @@
+// 2621. Sleep
+// Easy
+// 44
+// 1
+// Companies
+// Write an asyncronous function that accepts a positive integer millis and sleeps for that many milliseconds. It can resolve any value.
+
+// Example 1:
+
+// Input: millis = 100
+// Output: 100
+// Explanation: It should return a promise that resolves after 100ms.
+// let t = Date.now();
+// sleep(100).then(() => {
+//   console.log(Date.now() - t); // 100
+// });
+// Example 2:
+
+// Input: millis = 200
+// Output: 200
+// Explanation: It should return a promise that resolves after 200ms.
+
+
+// Constraints:
+
+// 1 <= millis <= 1000
+
+
+
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+  return new Promise(resolve => {
+      setTimeout(() => {
+          resolve();
+      }, millis);
+  })
+}
+
+/**
+* let t = Date.now()
+* sleep(100).then(() => console.log(Date.now() - t)) // 100
+*/
